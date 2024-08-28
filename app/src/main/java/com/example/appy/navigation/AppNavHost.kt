a5153ce6a6909375e99e1feb5030a5713e7647ab
sheilaokwisa544 <sheilaokwisa18@gmail.com>
2024-08-28 16:56:32 +0300

    k

--- app/src/main/java/com/example/appy/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/appy/navigation/AppNavHost.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.appy.ui.theme.screens.login.LoginScreen
 import com.example.appy.ui.theme.screens.signup.SignupScreen
 
 import com.example.sellapy.ui.theme.screens.Intent.IntentScreen
@@ -55,6 +56,9 @@ fun AppNavHost(
         }
         composable(ROUT_SIGNUP) {
             SignupScreen(navController = navController)
+        }
+        composable(ROUT_LOGIN) {
+            LoginScreen(navController = navController)
         }
 
 
--- app/src/main/java/com/example/appy/ui/theme/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/appy/ui/theme/screens/login/LoginScreen.kt
@@ -1,18 +1,20 @@
-package com.example.appy.ui.theme.screens.signup
+package com.example.appy.ui.theme.screens.login
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -26,15 +28,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.Magenta
 import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,25 +46,23 @@ import androidx.navigation.compose.rememberNavController
 import com.example.appy.R
 import com.example.appy.ui.theme.Bluee
 import com.example.appy.ui.theme.cccc
-import com.example.sellapy.navigation.ROUT_DETAIL
+import com.example.appy.ui.theme.ssss
+import com.example.sellapy.navigation.ROUT_SIGNUP
 
 
 @Composable
-fun SignupScreen(navController: NavController){
+fun LoginScreen(navController: NavController){
 
-    Column(
-
+    Column (
         modifier = Modifier
-            .paint(painterResource(id = R.drawable.img_20), contentScale = ContentScale.FillBounds)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .fillMaxSize()
+            .paint(painterResource(id = R.drawable.img_20), contentScale = ContentScale.FillBounds),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ){
 
 
-    ) {
-
-
-
-        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(10.dp))
 
         Image(
             painter = painterResource(id = R.drawable.img_9),
@@ -70,110 +71,100 @@ fun SignupScreen(navController: NavController){
         )
 
         Text(
-            text = "SellApy",
+            text = "Welcome Back!",
             fontSize = 70.sp,
             fontFamily = FontFamily.Cursive,
-            color = Blue
+            color = cccc
         )
-        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(10.dp))
 
         Text(
-            text = "Do not have an account? Create account",
+            text = "Already have an account? Please enter your credentials",
             fontSize = 20.sp,
-            fontFamily = FontFamily.SansSerif,
-            color = White
+            fontFamily = FontFamily.Default,
+            color = ssss,
+            textAlign = TextAlign.Center
         )
 
 
-        var name by remember { mutableStateOf("") }
         var email by remember { mutableStateOf("") }
         var password by remember { mutableStateOf("") }
-        var confpassword by remember { mutableStateOf("") }
+
+
+
 
         Spacer(modifier = Modifier.height(10.dp))
 
-
-        // text fields... normal and outlined
-        // outlined
-
-        OutlinedTextField(
-            value = name,
-            onValueChange ={name = it},
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 10.dp, end = 10.dp),
-            leadingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "", tint = Blue)}, // icons at the beginning
-            // trailing icon at the end
-            label = { Text(text = "full name",color = White)},
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-
-            // email
+        // email
         OutlinedTextField(
             value = email,
             onValueChange ={email = it},
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(start = 10.dp, end = 10.dp),
-            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "", tint = Blue)}, // icons at the beginning
+            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "", tint = Blue) }, // icons at the beginning
             // trailing icon at the end
-            label = { Text(text = "email address", color = White)},
+            label = { Text(text = "email address",color = White)},
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
         )
         Spacer(modifier = Modifier.height(10.dp))
 
 
-            // password
+        // password
         OutlinedTextField(
             value = password,
             onValueChange ={password = it},
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(start = 10.dp, end = 10.dp),
-            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "", tint = Blue)}, // icons at the beginning
+            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "", tint = Blue) }, // icons at the beginning
             // trailing icon at the end
-            label = { Text(text = "password", color = White)},
+            label = { Text(text = "password",color = White)},
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
             visualTransformation = PasswordVisualTransformation()
 
         )
         Spacer(modifier = Modifier.height(10.dp))
 
+        // row
 
-        OutlinedTextField(
-            value = confpassword,
-            onValueChange ={confpassword = it},
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 10.dp, end = 10.dp),
-            leadingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "", tint = Blue)}, // icons at the beginning
-            // trailing icon at the end
-            label = { Text(text = "confirm password", color = White)},
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
-        )
-        Spacer(modifier = Modifier.height(10.dp))
+        Row {
+                    // button user
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(Bluee)
+            ) {
+                Text(
+                    text = "login as a user",
 
-        Button(
-            onClick = { },
-            colors = ButtonDefaults.buttonColors(Bluee)
-        ) {
-            Text(
-                text = "create an account",
+                    )
+            }
 
-                )
+            Spacer(modifier = Modifier.width(10.dp))
+
+
+                    // button admin
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(Bluee)
+            ) {
+                Text(
+                    text = "login as an admin",
+
+                    )
+            }
         }
+        // end of row
 
-        Spacer(modifier = Modifier.height(10.dp))
+        Spacer(modifier = Modifier.height(5.dp))
 
         Text(
-            text = "Already have an account? Login",
+            text = "Do not have an account? Create account",
             fontSize = 20.sp,
             fontFamily = FontFamily.SansSerif,
-            color = cccc
+            color = Black,
+            modifier = Modifier.clickable { navController.navigate(ROUT_SIGNUP) }
         )
-
-
 
 
 
@@ -182,12 +173,10 @@ fun SignupScreen(navController: NavController){
 
     }
 
-
-
 }
 
 @Composable
 @Preview(showBackground = true)
-fun SignupScreenPreview(){
-    SignupScreen(rememberNavController())
+fun LoginScreenPreview(){
+    LoginScreen(rememberNavController())
 }
